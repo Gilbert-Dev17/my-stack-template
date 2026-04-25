33036e31c79a65f859c999635d7d30d8081f81ae
Meow <108517390+MeowMeow0417@users.noreply.github.com>
2026-04-25 13:09:14 +0800

    updated the proxy

--- proxy.ts
+++ proxy.ts
@@ -1,19 +1,20 @@
-import { NextRequest, NextResponse } from "next/server";
-import { updateSession } from "@/lib/supabase/proxy";
+import { type NextRequest } from 'next/server'
+import { updateSession } from '@/lib/supabase/proxy'
 
 export async function proxy(request: NextRequest) {
-    return await updateSession(request);
+  // update user's auth session
+  return await updateSession(request)
 }
 
 export const config = {
   matcher: [
     /*
-     * Match all request paths except:
+     * Match all request paths except for the ones starting with:
      * - _next/static (static files)
-     * - _next/image (image optimization)
-     * - favicon.ico
-     * - Public assets
+     * - _next/image (image optimization files)
+     * - favicon.ico (favicon file)
+     * Feel free to modify this pattern to include more paths.
      */
-    "/((?!_next/static|_next/image|favicon.ico|.*\\.(?:svg|png|jpg|jpeg|gif|webp)$).*)",
+    '/((?!_next/static|_next/image|favicon.ico|.*\\.(?:svg|png|jpg|jpeg|gif|webp)$).*)',
   ],
-};
+}
--- src/lib/actions/auth.ts
+++ src/lib/actions/auth.ts
@@ -1,3 +1,19 @@
 'use server'
 
-import { createClientClient } from '@/lib/supabase/server'
+import { createClient} from '@/lib/supabase/server'
+import { revalidatePath } from 'next/cache'
+
+export async function signIn() {
+    const supabase = await createClient()
+
+
+}
+
+export async function signOut() {
+    const supabase = await createClient()
+    await supabase.auth.signOut()
+}
+
+export async function signUp() {
+    const supabase = await createClient()
+}
